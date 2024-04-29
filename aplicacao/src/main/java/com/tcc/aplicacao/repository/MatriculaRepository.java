a09a7f0f0894decf5bb15faee00571483436567d
NaianeG <naianegoulartoliveira@gmail.com>
2024-04-29 09:51:26 -0300

    Aplication.properties, mudado

--- aplicacao/src/main/java/com/tcc/aplicacao/repository/MatriculaRepository.java
+++ aplicacao/src/main/java/com/tcc/aplicacao/repository/MatriculaRepository.java
@@ -1,6 +1,5 @@
 package com.tcc.aplicacao.repository;
 
-import com.tcc.aplicacao.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.tcc.aplicacao.entities.Matricula;
