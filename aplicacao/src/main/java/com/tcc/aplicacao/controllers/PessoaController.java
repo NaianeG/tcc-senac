b5a6cda32a9fa6a2d6d847652412a6a92a48be57
Lucas Moraes <lucasmoraes86@hotmail.com>
2024-05-13 16:59:19 -0300

    Ajustes na home e criação de formulário para criar docentes

--- aplicacao/src/main/java/com/tcc/aplicacao/controllers/PessoaController.java
+++ aplicacao/src/main/java/com/tcc/aplicacao/controllers/PessoaController.java
@@ -1,6 +1,7 @@
 package com.tcc.aplicacao.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,12 +9,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.tcc.aplicacao.entities.Pessoa;
 import com.tcc.aplicacao.services.PessoaService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping(value = "/pessoa")
+@Controller
 public class PessoaController {
 
     @Autowired
