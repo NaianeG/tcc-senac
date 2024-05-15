cc457b784ff13ac40e1e66ae5399187a15d14eb8
NaianeG <naianegoulartoliveira@gmail.com>
2024-05-15 11:02:15 -0300

    Arquivos para abrir os relatórios via aplicação e a listagem dos docentes

---
+++ aplicacao/src/main/java/com/tcc/aplicacao/controllers/JasperController.java
@@ -0,0 +1,41 @@
+package com.tcc.aplicacao.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.tcc.aplicacao.services.JasperService;
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServletResponse;
+
+@Controller
+public class JasperController {
+
+    @Autowired
+    private JasperService service;
+
+    @GetMapping("/listaDocentes")
+    public String abrir() {
+        return "listaDocentes";
+    }
+
+    @GetMapping("/relatorio/pdf/jr1")
+    public void exibirRelatorio01(@RequestParam("code") String code,
+            @RequestParam("acao") String acao,
+            HttpServletResponse response) throws IOException {
+
+        byte[] bytes = service.exportarPDF(code);
+        response.setContentType(MediaType.APPLICATION_PDF_VALUE);
+        if (acao.equals("v")) {
+            response.setHeader("Content-disposition", "incline; filename=relatorio-" + code + ".pdf");
+        } else {
+            response.setHeader("attachment", "incline; filename=relatorio-" + code + ".pdf");
+
+        }
+        response.getOutputStream().write(bytes);
+
+    }
+
+}
--- aplicacao/src/main/java/com/tcc/aplicacao/controllers/PessoaController.java
+++ aplicacao/src/main/java/com/tcc/aplicacao/controllers/PessoaController.java
@@ -1,6 +1,7 @@
 package com.tcc.aplicacao.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.amqp.RabbitProperties.Cache.Connection;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
