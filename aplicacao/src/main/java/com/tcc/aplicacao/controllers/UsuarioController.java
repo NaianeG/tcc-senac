0bf770b7bbb2d1acd3188c58408b52d9cbcc1bbd
Lucas Moraes <lucasmoraes86@hotmail.com>
2024-05-26 21:38:20 -0300

    Correção de serviços envolvendo a classe Usuario, remoção de comentários e construída lógica JS para apresentação dos Menus

--- aplicacao/src/main/java/com/tcc/aplicacao/controllers/UsuarioController.java
+++ aplicacao/src/main/java/com/tcc/aplicacao/controllers/UsuarioController.java
@@ -1,16 +1,12 @@
 package com.tcc.aplicacao.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.tcc.aplicacao.dto.CadastroDTO;
-import com.tcc.aplicacao.dto.UsuarioDto;
 import com.tcc.aplicacao.services.UsuarioService;
 
 @Controller
@@ -28,20 +24,5 @@ public class UsuarioController {
     public String cadastroUsuario(@Validated CadastroDTO cadastroDTO) {
         service.salvar(cadastroDTO);
         return "redirect:/login";
-    }
-
-    @GetMapping("/lista")
-    public List<UsuarioDto> listaUsuarios() {
-        return service.listaUsuario();
-    }
-
-    // @GetMapping("/busca/{id}")
-    // public UsuarioDto buscaUsuario(@PathVariable("id") int id) {
-    // return service.buscaUsuario(id);
-    // }
-
-    @GetMapping("/excluir/{id}")
-    public String excluirUsuario(@PathVariable("id") int id) {
-        return service.excluirUsuario(id);
     }
 }
--- aplicacao/src/main/java/com/tcc/aplicacao/security/SecurityConfig.java
+++ aplicacao/src/main/java/com/tcc/aplicacao/security/SecurityConfig.java
@@ -21,50 +21,6 @@ public class SecurityConfig {
     @Autowired
     private UsuarioDetailService usuarioDetailService;
 
-    // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-    // Exception {
-    // return http
-    // .csrf(csrf -> csrf.disable())
-    // .sessionManagement(session ->
-    // session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-    // .authorizeHttpRequests(authorize -> authorize
-    // // Colocar a rota para liberar para todos com permit all, ou bloquear
-    // // dependendo o tipo de usuario com hasRole
-
-    // .requestMatchers(HttpMethod.DELETE, "/deletarDocente/{id}").permitAll()
-    // .requestMatchers(HttpMethod.POST, "/cadastrarDocente").permitAll()
-    // .requestMatchers(HttpMethod.GET, "/cadastroDocente").permitAll()
-    // .requestMatchers(HttpMethod.GET, "css/formCadastroDocente.css").permitAll()
-    // .requestMatchers(HttpMethod.GET,
-    // "scripts/formCadastroDocente.js").permitAll()
-
-    // .requestMatchers(HttpMethod.GET, "/home").permitAll()
-    // .requestMatchers("/css/home.css").permitAll()
-    // .requestMatchers("/scripts/home.js").permitAll()
-
-    // .requestMatchers(HttpMethod.GET, "/login").permitAll()
-    // .requestMatchers(HttpMethod.POST, "/logar").permitAll()
-    // .requestMatchers(HttpMethod.GET, "/css/login.css").permitAll()
-    // .requestMatchers(HttpMethod.GET, "/css/listaDocente.css").permitAll()
-    // .requestMatchers("/css/listaDocente.css").permitAll()
-    // .requestMatchers("/css/home.css").permitAll()
-    // .requestMatchers("/scripts/home.js").permitAll()
-    // // .requestMatchers("/cadastro").hasRole("ADMIN")
-    // .requestMatchers(HttpMethod.POST, "/cadastro").permitAll()
-    // .requestMatchers(HttpMethod.GET, "/cadastro").permitAll()
-    // .requestMatchers(HttpMethod.GET, "/listaDocentes").permitAll()
-    // .requestMatchers(HttpMethod.POST, "/listaDocentes").permitAll()
-
-    // .requestMatchers(HttpMethod.POST,
-    // "/relatorio/pdf/relatorio-docente").permitAll()
-    // .requestMatchers(HttpMethod.GET,
-    // "/relatorio/pdf/relatorio-docente").permitAll()
-    // .anyRequest().authenticated())
-    // .formLogin(form -> form.loginPage("/login"))
-    // .build();
-    // }
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
