c9566e9db0fbc99344b8f0d708695d28b6b435c5
NaianeG <naianegoulartoliveira@gmail.com>
2024-06-11 20:32:09 -0300

    Alterado relatorio

--- aplicacao/src/main/java/com/tcc/aplicacao/controllers/JasperController.java
+++ aplicacao/src/main/java/com/tcc/aplicacao/controllers/JasperController.java
@@ -1,15 +1,12 @@
 package com.tcc.aplicacao.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.tcc.aplicacao.repository.MarcacaoPontoRepository;
 import com.tcc.aplicacao.repository.PessoaRepository;
 import com.tcc.aplicacao.services.JasperService;
 
