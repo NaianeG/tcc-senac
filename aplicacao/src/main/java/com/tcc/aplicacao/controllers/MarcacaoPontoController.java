da9e71affe2e843ad76edd1be5598f06869e04a3
IagoRamos1 <iago-ramos1000@hotmail.com>
2024-06-16 17:35:33 -0300

    Resolvendo problema de marcação de ponto, código de salvar ponto havia sido apagado

--- aplicacao/src/main/java/com/tcc/aplicacao/controllers/MarcacaoPontoController.java
+++ aplicacao/src/main/java/com/tcc/aplicacao/controllers/MarcacaoPontoController.java
@@ -1,5 +1,7 @@
 package com.tcc.aplicacao.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -8,9 +10,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.tcc.aplicacao.entities.MarcacaoPonto;
 import com.tcc.aplicacao.entities.Usuario;
@@ -23,6 +28,19 @@ public class MarcacaoPontoController {
 
     @Autowired
     private MarcacaoPontoService marcacaoPontoService;
+
+    @PostMapping("cadastroPonto")
+    public ModelAndView cadastroPonto(MarcacaoPonto marcacaoPonto, RedirectAttributes redirectAttributes) {
+
+        ModelAndView mv = new ModelAndView("redirect:/home");
+        List<MarcacaoPonto> marcacaoList;
+        marcacaoList = marcacaoPontoService.cadastroPonto(marcacaoPonto);
+        if (marcacaoList.isEmpty()) {
+            redirectAttributes.addFlashAttribute("alerta", "limiteAtingido");
+        }
+        mv.addObject("listaHoraMarcada", marcacaoList);
+        return mv;
+    }
 
     @GetMapping("listaPonto/{id}")
     public String listaPonto(@PathVariable int id,
