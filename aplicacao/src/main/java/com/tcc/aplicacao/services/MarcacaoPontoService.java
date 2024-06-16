c95315cfedcedcb5ffa0b411b3de0fa8f54cc510
Lucas Moraes <lucasmoraes86@hotmail.com>
2024-06-16 19:08:57 -0300

    Ajustando recursividade da página de listagem de ponto

--- aplicacao/src/main/java/com/tcc/aplicacao/services/MarcacaoPontoService.java
+++ aplicacao/src/main/java/com/tcc/aplicacao/services/MarcacaoPontoService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import com.tcc.aplicacao.entities.MarcacaoPonto;
