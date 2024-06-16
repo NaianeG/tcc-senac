3ae4d6161eb4f93b7a43417394169f9c9bc8ff99
IagoRamos1 <iago-ramos1000@hotmail.com>
2024-06-16 17:23:11 -0300

    Ajuste do modal

--- aplicacao/src/main/resources/static/scripts/listaPonto.js
+++ aplicacao/src/main/resources/static/scripts/listaPonto.js
@@ -1,31 +1,31 @@
 function toggleMenu() {
-    const menuMobile = document.getElementById('menu-mobile');
-    const isMenuActive = menuMobile.getAttribute('aria-hidden') === 'false';
-    menuMobile.setAttribute('aria-hidden', !isMenuActive);
-    if (isMenuActive) {
-        menuMobile.classList.remove('menu-mobile-active');
+    let menu = document.getElementById("menu-mobile");
+    let isHidden = menu.getAttribute("aria-hidden") === "true";
+    menu.setAttribute("aria-hidden", !isHidden);
+    if (isHidden) {
+        menu.classList.add("menu-mobile-active");
     } else {
-        menuMobile.classList.add('menu-mobile-active');
+        menu.classList.remove("menu-mobile-active");
     }
 }
 
 function confirmDelete(id) {
-    const modal = document.getElementById('confirmModal');
+    let modal = document.getElementById('confirmModal');
     modal.style.display = 'block';
 
-    const confirmBtn = document.getElementById('confirmBtn');
+    let confirmBtn = document.getElementById('confirmBtn');
     confirmBtn.onclick = function() {
         window.location.href = `/deletarPonto/${id}`;
     };
 }
 
 function closeModal() {
-    const modal = document.getElementById('confirmModal');
+    let modal = document.getElementById('confirmModal');
     modal.style.display = 'none';
 }
 
 window.onclick = function(event) {
-    const modal = document.getElementById('confirmModal');
+    let modal = document.getElementById('confirmModal');
     if (event.target == modal) {
         modal.style.display = 'none';
     }
